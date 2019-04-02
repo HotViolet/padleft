9ced6e4ded0569bf5083d0574ef82ac0de00a796
HotViolet <jwblues@hotmail.co.uk>
2019-04-02 20:53:32 +0100

    Update padleft.cpp

--- src/padleft.cpp
+++ src/padleft.cpp
@@ -1,16 +1,31 @@
 
+
 #include <padleft.hpp>
+
 
 namespace pl {
 
 
-std::string pad_left(const std::string& str, int num_spaces)
-{
-    std::string out = str;
-    for (int i = 0; i < num_spaces; i++) {
-        out = " " + out; // this is rubbish ;)
+    std::string pad_left(const std::string& str, int num_spaces)
+
+    {
+
+        std::string spc = "";
+        std::string out = "";
+
+        for (int i = 0; i < num_spaces; i++) {
+
+            spc += " ";
+
+        }
+
+//        out = spc + out;
+        out = spc + str;
+
+        return out;
+
     }
-    return out;
-}
+
 
 }
+
